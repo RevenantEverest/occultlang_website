b368fc081183ff10ec86392201804018797af2e1
Stefan Fischer <stefischer@mail.com>
2024-12-12 05:30:14 -0500

    Adjust Layout styling

--- src/components/Common/Layout.tsx
+++ src/components/Common/Layout.tsx
@@ -7,7 +7,21 @@ interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
 function Layout({ className, children }: React.PropsWithChildren<LayoutProps>) {
     return(
         <React.Fragment>
-            <div className={`flex flex-col items-center h-full ${className}`}>
+            <div 
+                className={`
+                    flex-col
+                    w-full
+                    relative 
+                    px-5 
+                    lg:px-52
+                    pt-20 
+                    pointer-events-auto 
+                    items-center
+                    justify-center
+                    bg-background
+                    ${className}
+                `}
+            >
                 {children}
             </div>
         </React.Fragment>
