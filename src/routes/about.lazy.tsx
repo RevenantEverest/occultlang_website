4e4b07fd7e7bd32f958627d4762f25aa670e7296
Stefan Fischer <stefischer@mail.com>
2024-12-12 05:30:24 -0500

    Add Layout to about route

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,13 +1,14 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 
+import Layout from '@@components/Common/Layout';
+
 export const Route = createLazyFileRoute('/about')({
     component: About,
 });
 
 function About() {
     return(
-      <div className="p-2">
-        Hello from About!
-      </div>
+        <Layout className="items-center justify-center">
+        </Layout>
     );
 };
