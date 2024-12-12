7e680b5a1e155b5e00cd46cde9f6871a7a2df11a
Stefan Fischer <stefischer@mail.com>
2024-12-12 05:32:00 -0500

    Add Navbar and Footer to _root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,18 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
+import Navbar from '@@navigation/Navbar';
+import Footer from '@@navigation/Footer';
+
 import ThemeValidator from '@@components/ThemeChanger/ThemeValidator';
 
 export const Route = createRootRoute({
     component: () => (
-        <div className="h-[100vh] bg-neutral-800 text-white">
+        <div className="min-h-[100vh] text-text">
             <ThemeValidator />
+            <Navbar />
             <Outlet />
+            <Footer />
             <TanStackRouterDevtools />
         </div>
     ),
