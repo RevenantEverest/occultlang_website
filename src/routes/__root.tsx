a13cb878c807e55dd88ca48237dc72bccd484d5a
Stefan Fischer <stefischer@mail.com>
2024-12-12 01:32:04 -0500

    Add ThemeValidator to _root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
+import ThemeValidator from '@@components/ThemeChanger/ThemeValidator';
+
 export const Route = createRootRoute({
     component: () => (
         <div className="h-[100vh] bg-neutral-800 text-white">
+            <ThemeValidator />
             <Outlet />
             <TanStackRouterDevtools />
         </div>
