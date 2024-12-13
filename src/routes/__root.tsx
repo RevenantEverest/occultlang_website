92fa0976a019917011f8b0adc694609960f68f6a
Stefan Fischer <stefischer@mail.com>
2024-12-13 05:14:37 -0500

    Add tanstack router devtools conditional render

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { createRootRoute, useMatches, useMatch } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import AnimatedOutlet from '@@components/AnimatedOutlet';
 import { AnimatePresence } from 'motion/react';
@@ -8,6 +8,18 @@ import Navbar from '@@navigation/Navbar';
 import Footer from '@@navigation/Footer';
 
 import ThemeValidator from '@@components/ThemeChanger/ThemeValidator';
+
+const TanStackRouterDevtools =
+    process.env.NODE_ENV === "production"
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+            // Lazy load in development
+            import('@tanstack/router-devtools').then((res) => ({
+                default: res.TanStackRouterDevtools,
+                // For Embedded Mode
+                // default: res.TanStackRouterDevtoolsPanel
+            })),
+        );
 
 export const Route = createRootRoute({
     component: Root,
