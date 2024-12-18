174a50600a863d83bb6cb3e58ac6c3f23a7544a0
Stefan Fischer <stefischer@mail.com>
2024-12-17 19:20:16 -0500

    Add links to footer icons

--- src/navigation/Footer.tsx
+++ src/navigation/Footer.tsx
@@ -1,13 +1,15 @@
 import { FaGithub, FaDiscord } from 'react-icons/fa';
 import { motion } from 'motion/react';
 
+import { URLS } from '@@constants';
+
 function Footer() {
 
     return(
         <div className="flex flex-col relative">
             <div className="absolute bg-card top-0 h-5 w-full blur-md" />
             <div className="flex bg-background justify-center py-5 px-4 md:px-50 z-20">
-                <div className="flex flex-1 z-10">
+                <div className="flex flex-1 z-10 items-center">
                     <div className="flex-1 justify-start">
                         <p className="text-sm">Copyright © {new Date().getFullYear()} Occultlang - Apache License</p>
                     </div>
@@ -16,13 +18,17 @@ function Footer() {
                             className="hover:cursor-pointer"
                             whileHover={{ y: "-.5vh" }}
                         >
-                            <FaDiscord />
+                            <a href={URLS.DISCORD_SERVER} target="_blank" rel="noopener noreferrer">
+                                <FaDiscord />
+                            </a>
                         </motion.div>
                         <motion.div
                             className="hover:cursor-pointer"
                             whileHover={{ y: "-.5vh" }}
                         >
-                            <FaGithub />
+                            <a href={URLS.GITHUB_REPO} target="_blank" rel="noopener noreferrer">
+                                <FaGithub />
+                            </a>
                         </motion.div>
                     </div>
                 </div>
